6ccf944daeb46f9009fdb37ebf3a2ebcd9ea84c8
s0ham075 <sohamsawant075@gmail.com>
2024-01-04 04:25:12 -0800

    ocr verification:

--- UniConnect Frontend/uniconnect/src/app/_components/uploadButton.jsx
+++ UniConnect Frontend/uniconnect/src/app/_components/uploadButton.jsx
@@ -4,27 +4,40 @@
 import "@uploadthing/react/styles.css";
 import Tesseract from "tesseract.js";
 import { UploadButton } from "@uploadthing/react";
- 
-export default function UploadButtonComponent() {
-  
+import { useState } from "react";
+import axios from "axios";
+export default function UploadButtonComponent(props) {
+  const [verificationStatus , SetVerificationStatus] = useState(false)
+  const [ocrText,setOcrText] = useState('');
+  const [status, setStatus] = useState('')
+  const verify = async()=>{
+     const response = await axios.post("http://localhost:4000/verify",{ institute : props.institute , ocrText});
+     setStatus(response.data.message)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex  flex-col items-center justify-between p-4">
       <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
           console.log("Files: ",res[0].url);
           alert("Upload Completed");
-
+          SetVerificationStatus(true)
           Tesseract.recognize(
             res[0].url,
             'eng',
-          ).then(({ data: { text } }) => {
-            console.log(text);
+          ).then(async({ data: { text } }) => {
+            console.log(text.toLowerCase());
+            setOcrText(text.toLowerCase())
+            const response = await verify();
+            console.log(response)
+            SetVerificationStatus(false)
           })
           
         }}
       />
+      { verificationStatus && <h3 className="font-BOLD text-white">Verifying</h3>}
+      <h2>{status}</h2>
     </main>
   );
 }
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app")
 const {connectDatabase} = require("./config/database");
 require('dotenv').config();
 
-connectDatabase();
+// connectDatabase();
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 })
