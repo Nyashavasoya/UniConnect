a3fe766f4e61c00e7d8bc5184313da08026ef744
s0ham075 <sohamsawant075@gmail.com>
2024-01-03 21:49:18 -0800

    tessaract ocr added

--- UniConnect Frontend/uniconnect/src/app/_components/uploadButton.jsx
+++ UniConnect Frontend/uniconnect/src/app/_components/uploadButton.jsx
@@ -2,16 +2,27 @@
  
 // You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
 import "@uploadthing/react/styles.css";
- 
+import Tesseract from "tesseract.js";
 import { UploadButton } from "@uploadthing/react";
  
 export default function UploadButtonComponent() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UploadButton
         endpoint="imageUploader"
-        onClientUploadComplete={() => {
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ",res[0].url);
           alert("Upload Completed");
+
+          Tesseract.recognize(
+            res[0].url,
+            'eng',
+          ).then(({ data: { text } }) => {
+            console.log(text);
+          })
+          
         }}
       />
     </main>
